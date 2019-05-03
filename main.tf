95f9503ccd7ea4c43d759a3d6e142dee24e04344
Les Aker <me@lesaker.org>
2019-05-03 14:45:12 +0000

    set empty images

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ resource "linode_stackscript" "this" {
   label       = "deploy-wireguard"
   description = "Deploy wireguard"
   script      = "${file("${path.module}/assets/stackscript.sh")}"
+  images      = []
 }
